d8271af7a894cbd8230068f2427e71a9b8c712cf
Frank Capraro <Frank@Franks-MacBook-Pro.local>
2016-06-15 22:09:52 -0400

    was able append all new table information. Now need to work on how the molar js features

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,14 +7,10 @@ var frequency = '';
 
 	$('#submit').on('click',function(){
 		// take the values of the inputed information 
-		name = $('#nameInput').val();
-		console.log(name);
+		name = $('#trainName').val();
 		destination = $('#destinationInput').val();
-		console.log(destination);
 		time = $('#firstTrainTimeInput').val();
-		console.log(time);
 		frequency = $('#frequencyInput').val();
-		console.log(frequency);
 		// push info to firebase
 		trainData.push({
 			name: name,
@@ -29,34 +25,42 @@ var frequency = '';
 		$('#firstTrainTimeInput').val('');
 		$('#frequencyInput').val('');
 		// dont refresh the page 
-		debugger;
 
 		return false;
 	});
 // use the recent append child to learn append information. 
-/*trainData.on("child_added", function(snapshot) {
-	// Log everything that's coming out of snapshot
-	console.log(snapshot.val().name);
-	console.log(snapshot.val().destination);
-	console.log(snapshot.val().time);
-	console.log(snapshot.val().frequency);
-	console.log(snapshot.val().dateAdded);
-	// Change the HTML to reflect
-	var tableRow = $('<tr>');
-	var tableData1 = $('<td>');
-	var tableData2 = $('<td>');
-	var tableData3 = $('<td>');
-	var tableData4 = $('<td>');
-	tableData1.html(snapshot.val().name);
-	tableData2.html(snapshot.val().email);
-	tableData3.html(snapshot.val().age);
-	tableData4.html(snapshot.val().comment);
+trainData.on("child_added", function(snapshot) {
 
-	tableRow.append(tableData1,tableData2,tableData3,tableData4);
-	$('.t1').append(tableRow);
+	// Log everything that's coming out of snapshot
+	console.log(snapshot.val().name + " name");
+	console.log(snapshot.val().destination + "destination");
+	console.log(snapshot.val().time + " time");
+	console.log(snapshot.val().frequency +" frequency");
+
+	// Create a new Table row. outside of the for loop 	
+	var tableRow = $('<tr>');	
+	// add all train information to an array for easy use 
+	var trainInfo = [snapshot.val().name, snapshot.val().destination, snapshot.val().time, snapshot.val().frequency];
+	console.log(trainInfo);
+	//create a for loop to easily create a new table data with  the HTMl from the inputed value 
+	for(var i = 0; i < trainInfo.length;i++){
+		// create a new td
+		var tableData = $('<td>');
+		// add the information from the array to the HTMl of this td
+		tableData.html(trainInfo[i]);
+		console.log(tableData);
+		// give it an attribute for save measure 
+		tableData.attr('id',i);
+		// append current td to table row 
+		tableRow.append(tableData)
+	}
+	// append the new row to the last child of the table body
+	$('#trainInfo > tBody:last-child').append(tableRow);
+	// return false to prevent constant returning
+	return false;
 // Handle the errors
 }, function(errorObject){
 	console.log("Errors handled: " + errorObject.code)
-})*/
+})
 
 
